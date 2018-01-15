ba137c4945350ac7afbc2a03f1d7dd66077354c2
Destin Moulton <destin.moulton@gmail.com>
2018-01-15 11:52:31 -0700

    Added POST dynamoDB interaction to add a log to the db.

--- handler.ts
+++ handler.ts
@@ -1,18 +1,44 @@
 import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
+import * as uuid from "uuid";
+
+const logTableName = "GizmoVaultLog";
+const db = new DynamoDB.DocumentClient();
 
 export const logPost: Handler = (
     event: APIGatewayEvent,
     context: Context,
     cb: Callback
 ) => {
-    console.log("logPost called");
-    console.log(event);
+    const timestamp = new Date().getTime();
+
+    const data = JSON.parse(event.body);
+
+    const type = data.type || "log";
+    const app = data.app || "Unknown App";
+    const info = data.info || data;
+
+    const params = {
+        TableName: logTableName,
+        Item: {
+            id: uuid.v1(),
+            app,
+            type,
+            info,
+            event,
+            createdAt: timestamp
+        }
+    };
+
+    db.put(params, error => {
+        if (error) console.error(error);
+    });
+
+    // Always return a positive response.
     const response = {
         statusCode: 200,
-        body: JSON.stringify({
-            message: "logPost called!",
-            input: event
-        })
+        headers: { "Content-Type": "text/plain" },
+        body: "Added log item."
     };
 
     cb(null, response);
